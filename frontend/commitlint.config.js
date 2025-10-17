24d8e28a8fd60d286759c8b819d602d6e6c04be1
Joel Bystedt <joel.bystedt@gmail.com>
2025-10-17 15:54:35 +0200

    (wow) feat: init files

---
+++ frontend/commitlint.config.js
@@ -0,0 +1,50 @@
+export default {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^\(([^\)]+)\) ([a-z]+): (.+)$/,
+      headerCorrespondence: ['scope', 'type', 'subject']
+    }
+  },
+  plugins: [
+    {
+      rules: {
+        'custom-format': (parsed) => {
+          const { scope, type, subject } = parsed;
+
+          if (!scope || !type || !subject) {
+            return [
+              false,
+              'Commit message must follow format: (feature) commit-type: message'
+            ];
+          }
+
+          const validTypes = [
+            'feat',
+            'fix',
+            'docs',
+            'style',
+            'refactor',
+            'perf',
+            'test',
+            'build',
+            'ci',
+            'chore',
+            'revert'
+          ];
+
+          if (!validTypes.includes(type)) {
+            return [
+              false,
+              `Commit type must be one of: ${validTypes.join(', ')}`
+            ];
+          }
+
+          return [true, ''];
+        }
+      }
+    }
+  ],
+  rules: {
+    'custom-format': [2, 'always']
+  }
+};
